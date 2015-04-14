85f4b2d88b86500faadbb96eaeadf2a6d4ac22f3
Hugh <howhelan@wpi.edu>
2015-04-13 22:05:15 -0400

    Added back button to game mode select

--- 6isWild/src/boundaries/SelectGameModeDisplay.java
+++ 6isWild/src/boundaries/SelectGameModeDisplay.java
@@ -21,27 +21,36 @@ public class SelectGameModeDisplay extends JPanel {
 		
 		JLabel lblSelectAGame = new JLabel("SELECT A GAME MODE");
 		lblSelectAGame.setFont(new Font("Lucida Grande", Font.PLAIN, 45));
+		
+		JButton btnBack = new JButton("Back");
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(147)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblSelectAGame)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(btnNewButton)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(btnElimination, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(btnPuzzle, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(btnRelease, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)))
+							.addGap(147)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblSelectAGame)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(btnNewButton)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(btnElimination, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(btnPuzzle, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(btnRelease, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE))))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(22)
+							.addComponent(btnBack)))
 					.addContainerGap(157, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(63)
+					.addGap(22)
+					.addComponent(btnBack, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(lblSelectAGame)
 					.addPreferredGap(ComponentPlacement.RELATED, 67, Short.MAX_VALUE)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
