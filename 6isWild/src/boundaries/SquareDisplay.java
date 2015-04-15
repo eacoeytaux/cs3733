2f5c4bee8a8e9272e29b2fd793ebc2b444d5b06b
Hugh <howhelan@wpi.edu>
2015-04-14 22:29:22 -0400

    trying tile and square display

--- 6isWild/src/boundaries/SquareDisplay.java
+++ 6isWild/src/boundaries/SquareDisplay.java
@@ -1,5 +1,28 @@
 package boundaries;
 
-public class SquareDisplay {
+import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+
+public class SquareDisplay extends JPanel {
+	public SquareDisplay() {
+		
+		tile = new TileDisplay();
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(tile, GroupLayout.DEFAULT_SIZE, 56, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(tile, GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		setLayout(groupLayout);
+	}
 	TileDisplay tile;
 }
--- 6isWild/src/boundaries/TileDisplay.java
+++ 6isWild/src/boundaries/TileDisplay.java
@@ -1,5 +1,40 @@
 package boundaries;
 
-public class TileDisplay {
+import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
+public class TileDisplay extends JPanel {
+	public TileDisplay() {
+		
+		JLabel label = new JLabel("1");
+		label.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
+		
+		JLabel lblX = new JLabel("x2");
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblX)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(20)
+							.addComponent(label)))
+					.addContainerGap(21, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(lblX)
+					.addContainerGap(44, Short.MAX_VALUE))
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap(18, Short.MAX_VALUE)
+					.addComponent(label)
+					.addContainerGap())
+		);
+		setLayout(groupLayout);
+	}
 }
