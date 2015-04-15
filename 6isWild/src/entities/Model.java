68f6dd000865c5b1cfbdcd7d3bcde30633814af4
eacoeytaux <eacoeytaux@gmail.com>
2015-04-15 18:52:14 -0400

    Added model class

---
+++ 6isWild/src/entities/Model.java
@@ -0,0 +1,20 @@
+package entities;
+
+import java.util.ArrayList;
+
+public class Model {
+	ArrayList<PuzzleLevel> puzzleLevels;
+	ArrayList<LightningLevel> lightningLevels;
+	ArrayList<EliminationLevel> eliminationLevels;
+	ArrayList<ReleaseLevel> releaseLevels;
+	GlobalStats globalStats;
+	
+	public Model() {
+		puzzleLevels = new ArrayList<PuzzleLevel>();
+		puzzleLevels.add(0, new PuzzleLevel());
+		lightningLevels = new ArrayList<LightningLevel>(); 
+		eliminationLevels = new ArrayList<EliminationLevel>(); 
+		releaseLevels = new ArrayList<ReleaseLevel>(); 
+		globalStats = new GlobalStats(0,0,0);	
+	}
+}
